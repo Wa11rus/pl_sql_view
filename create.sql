c648922433d1dd4265e15477ab455cfab6bf347f
Wa11rus <44881039+Wa11rus@users.noreply.github.com>
2020-05-04 20:24:15 +0300

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE locations (
+    location_id   INTEGER NOT NULL PRIMARY KEY,
+    latitude      FLOAT NOT NULL UNIQUE,
+    longitude     FLOAT NOT NULL UNIQUE
+);
+
+CREATE TABLE params (
+    params_id    INTEGER NOT NULL PRIMARY KEY,
+    brightness   FLOAT NOT NULL,
+    frp          FLOAT NOT NULL
+);
+
+CREATE TABLE confidence (
+    confidence INTEGER NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE fire_info (
+    fire_id       INTEGER NOT NULL PRIMARY KEY,
+    location_id   INTEGER NOT NULL UNIQUE,
+    params_id     INTEGER NOT NULL UNIQUE,
+    confidence    INTEGER NOT NULL
+);
+
+ALTER TABLE fire_info
+    ADD CONSTRAINT fk_location_id FOREIGN KEY ( location_id )
+        REFERENCES locations ( location_id );
+
+ALTER TABLE fire_info
+    ADD CONSTRAINT fk_params_id FOREIGN KEY ( params_id )
+        REFERENCES params ( params_id );
+
+ALTER TABLE fire_info
+    ADD CONSTRAINT fk_confidence FOREIGN KEY ( confidence )
+        REFERENCES confidence ( confidence );
